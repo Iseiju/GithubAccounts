c12cffd58fe81c70121f51f768b3ade2551e61f7
Kenneth James Uy <uy.kennethjames23@gmail.com>
2021-04-29 17:12:32 +0800

    Added APIClient and getRequest function for reusability

---
+++ GithubAccounts/api/APIClient.swift
@@ -0,0 +1,20 @@
+//
+//  APIClient.swift
+//  GithubAccounts
+//
+//  Created by Kenneth James Uy on 4/29/21.
+//
+
+import Alamofire
+import Foundation
+
+class APIClient {
+  
+  static let shared: APIClient = { return APIClient() }()
+  
+  func getRequest<T: Codable>(url: String,
+                              model: T.Type,
+                              completion: @escaping (_ response: DataResponse<T, AFError>) -> Void) {
+    AF.request(url, method: .get).responseDecodable(of: model, completionHandler: completion)
+  }
+}
