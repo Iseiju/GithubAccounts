d171bc9b63a4189c146d6efd63a1f83c9ee372b1
Kenneth James Uy <uy.kennethjames23@gmail.com>
2021-04-29 17:28:35 +0800

    Added UserListViewModel and GET request for user list

---
+++ GithubAccounts/viewmodels/UserListViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  UserListViewModel.swift
+//  GithubAccounts
+//
+//  Created by Kenneth James Uy on 4/29/21.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+class UserListViewModel {
+  
+  private let userList = BehaviorRelay<[User]>(value: [])
+  
+  // MARK: - Main API request
+  func getUsers(completion: @escaping (_ isSuccess: Bool, _ errorOrNil: Error?) -> Void) {
+    let url = "https://api.github.com/users"
+    
+    APIClient.shared.getRequest(url: url, model: [User].self, completion: { response in
+      switch response.result {
+      case .success(let users):
+        self.userList.accept(users)
+        completion(true, nil)
+        
+      case .failure(let error as Error):
+        completion(false, error)
+      }
+    })
+  }
+}
